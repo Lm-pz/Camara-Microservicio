7750fdfbda6ad651c0d0443d05fb0ea6f4b9b6a7
Laura Romero <lauram.romeropz@gmail.com>
2020-02-17 08:55:01 +0100

    First step

---
+++ src/main/java/com/example/CamaraMicroservicio/CamaraMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.example.CamaraMicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamaraMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamaraMicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CamaraMicroservicio/CamaraMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CamaraMicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamaraMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
